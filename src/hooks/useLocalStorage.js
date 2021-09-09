44cded4939f38c556b3c0bc7554184fce6420157
Moonlightsculptor88 <mahipreetham@gmail.com>
2021-09-09 12:16:34 +0530

    init

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,27 @@
+import React , {useEffect, useState} from 'react'
+
+ const PREFIX = 'editoor-'
+
+export default function useLocalStorage(key , initialValue) {
+    const prefixKey = PREFIX + key;
+
+    const [value, setValue] = useState(() =>{
+        const jsonValue = localStorage.getItem(prefixKey)
+
+        if (jsonValue != null) return JSON.parse(jsonValue)
+
+        if (typeof initialValue === 'function'){
+            return initialValue()
+        }else {
+            return initialValue
+        }
+
+    })
+
+    useEffect(()=>{
+        localStorage.setItem(prefixKey, JSON.stringify(value))
+    }, [prefixKey, value])
+
+
+    return [value, setValue]
+}
